1fec4ca792faea53ce3862d024f0bf05a486f825
Max Christy <tchristy001@outlook.com>
2021-12-15 21:26:55 +0000

    rework sdbus-cpp package

--- ports/sdbus-cpp/portfile.cmake
+++ ports/sdbus-cpp/portfile.cmake
@@ -1,21 +1,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kistler-Group/sdbus-cpp
-    REF v0.9.0
+    REF v1.0.0
     HEAD_REF master
-    SHA512 8695d3efac5654cb28998cdd69c16f251654b2c95e0dc2a2f5a4b01b24dae7b04e1a1f2e9bc9fb96b99f099dd4a130ca980cb672f74829565fd0d48162a581c5
+    SHA512 dc6b6c4945f5203ad5e4d86ae78088ce367a5ee28f3bb1c17c116c0755d9b221685a92b32dd9e354351067b012768e8d42ebe5dac93199d1384b9b4dadec09ef
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-      -D BUILD_TESTS=OFF
       -D BUILD_DOC=OFF
+      -D BUILD_EXAMPLES=OFF
+      -D BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sdbus-c++)
 vcpkg_fixup_pkgconfig()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
